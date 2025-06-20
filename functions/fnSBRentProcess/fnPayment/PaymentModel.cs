f329994800e46cbcd2103b539071676064c7d581
Adriano Santana <asantana8@gmail.com>
2025-06-20 02:37:26 -0300

    Incluindo fontes em .net

---
+++ functions/fnSBRentProcess/fnPayment/PaymentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fnPayment
+{
+    internal class PaymentModel
+    {
+        public Guid id { get { return Guid.NewGuid(); } }
+        public Guid idPayment { get { return Guid.NewGuid(); } }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string modelo { get; set; }
+        public int ano { get; set; }
+        public string tempoAluguel { get; set; }
+        public DateTime data { get; set; }
+        public String status { get; set; }
+        public DateTime? dataAprovacao { get; set; }
+    }
+}
